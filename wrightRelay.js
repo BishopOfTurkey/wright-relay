a3fd3602bab2a1d05a9efdc7e1e5b4d9665c405c
Obi Symons <u6951952@anu.edu.au>
2019-03-29 19:58:44 +1100

    wow i'm lazy

--- wrightRelay.js
+++ wrightRelay.js
@@ -6,7 +6,7 @@ login({email: "wrightrelay@protonmail.com", password: "RjF81ZJs!la&qT8wS"}, (err
 
     api.listen((err, message) => {
         console.log(message)
-        if (err) return console.err(err);
+        if (err) return console.error(err);
 
         if (message.threadID == '2230705296989694') { // GC1
             sendWithName('2050336258412519');
@@ -16,9 +16,25 @@ login({email: "wrightrelay@protonmail.com", password: "RjF81ZJs!la&qT8wS"}, (err
         
         function sendWithName (chat) {
             id = message.senderID
+
             api.getUserInfo(id, (err, ret) => {
-                if (err) return console.err(err);
-                api.sendMessage(ret[id].firstName + ': ' + message.body, chat);
+                if (err) return console.error(err);
+                
+                let newMsg= {
+                    body: ret[id].firstName + ': ' + message.body
+                }
+
+                api.sendMessage(newMsg, chat, (err, info) => {
+                    if (err) return console.error(err);
+                    if (message.attachments.length != 0) {
+                        message.attachments.forEach((i, arr) => {
+                            if (arr[i].type == 'photo') {
+                                api.forwardAttachment(arr[i].ID, chat);
+                            }
+                        }) 
+                    }
+                });
+                
             });
         }
     });
